a08e241305a9f7e1766d5843348804006abfd9bf
Jakub <gudzikoff@gmail.com>
2019-10-15 22:00:30 +0200

    1.3 Fizz, Buzz, FizzBuzz!

--- venv/Include/AI.py
+++ venv/Include/AI.py
@@ -1,13 +1,15 @@
-for i in range(2):
-    print("  |  |  H  |  |  H  |  |  ")
-    print("--+--+--H--+--+--H--+--+--")
-    print("  |  |  H  |  |  H  |  |  ")
-    print("--+--+--H--+--+--H--+--+--")
-    print("  |  |  H  |  |  H  |  |  ")
-    print("========+========+========")
+sum=0
+for i in range(1,41):
+    if i%3 == 0 or i%5==0:
+        print(i)
+        sum += i
 
-print("  |  |  H  |  |  H  |  |  ")
-print("--+--+--H--+--+--H--+--+--")
-print("  |  |  H  |  |  H  |  |  ")
-print("--+--+--H--+--+--H--+--+--")
-print("  |  |  H  |  |  H  |  |  ")
+print("Suma do 41: ")
+print (sum)
+
+sum=0
+for i in range(1,1001):
+    if i%3 == 0 or i%5==0:
+        sum += i
+print("Suma do 1001: ")
+print (sum)
